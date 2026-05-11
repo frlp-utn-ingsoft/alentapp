e0b6011f0b73e2e6abcd5b61fbddd6137375d9c2
yamartinez03 <yamartinez@frba.utn.edu.ar>
2026-05-11 00:49:32 -0300

    feat(Sport): agrega método findAll en SportRepository y PostgresSportRepository

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -6,4 +6,5 @@ import { SportDTO } from '@alentapp/shared';
 export interface SportRepository {
   create(sport: Omit<SportDTO, 'id'>): Promise<SportDTO>;
   findByName(name: string): Promise<SportDTO | null>;
+  findAll(): Promise<SportDTO[]>;
 }
