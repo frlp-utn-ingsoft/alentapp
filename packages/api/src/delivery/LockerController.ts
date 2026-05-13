65c6ea8f63b5deeb6ce7e4cff15afebc070c1e51
Tassi <tassi.tbj@gmail.com>
2026-05-13 15:43:14 -0300

    feat(locker): agregar método getall al controller

--- packages/api/src/delivery/LockerController.ts
+++ packages/api/src/delivery/LockerController.ts
@@ -1,10 +1,15 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { CreateLockerUseCase } from '../application/CreateLockerUseCase.js';
 import { CreateLockerRequest } from '@alentapp/shared';
+import { GetLockersUseCase } from '../application/GetLockersUseCase.js';
+import { GetLockersFilters } from '@alentapp/shared';
 
 export class LockerController {
     constructor(
         private readonly createLockerUseCase: CreateLockerUseCase,
+        private readonly getLockersUseCase: GetLockersUseCase,
+        
+
     ) {}
 
     async create(
@@ -28,4 +33,21 @@ export class LockerController {
             return reply.status(500).send({ error: 'Error interno, reintente más tarde' });
         }
     }
+    
+    async getAll(
+        request: FastifyRequest<{ Querystring: GetLockersFilters }>,
+        reply: FastifyReply,
+    ) {
+        try {
+            const { estado, ubicacion } = request.query;
+            const lockers = await this.getLockersUseCase.execute({ estado, ubicacion });
+            return reply.status(200).send(lockers);
+        } catch (error: any) {
+            if (error.message === 'Filtro inválido') {
+                return reply.status(400).send({ error: error.message });
+            }
+            return reply.status(500).send({ error: 'Error interno, reintente más tarde' });
+        }
+    }
+
 }
