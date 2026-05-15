a99cae992ae4a656ca0c3bcf121b2ed78bc9b13e
Dana Natasha Cadabon <natashacadabon@gmail.com>
2026-05-15 08:46:21 -0300

    feat(create-payment): agrega el puerto de salida PaymentRepository

---
+++ packages/api/src/domain/PaymentRepository.ts
@@ -0,0 +1,11 @@
+import { PaymentDTO } from '@alentapp/shared';
+
+// Esta interfaz es el "Puerto de Salida". El dominio dice: 
+// "No me importa si usás Postgres o Mongo, dame un objeto que cumpla esto".
+
+export interface PaymentRepository {
+    create(payment: Omit<PaymentDTO, 'id'>): Promise<PaymentDTO>;
+    findById(id: string): Promise<PaymentDTO | null>;
+    findByMemberId(member_id: string): Promise<PaymentDTO[]>;
+    findAll(): Promise<PaymentDTO[]>;
+}
