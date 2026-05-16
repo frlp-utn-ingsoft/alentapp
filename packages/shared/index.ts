e2c8bf0ec9203bed3612e94d02e03b14d7fc3e7c
Dana Natasha Cadabon <natashacadabon@gmail.com>
2026-05-16 01:38:08 -0300

    feat(payment): agrega interface updatePaymentRequest

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -5,31 +5,31 @@ export type MemberCategory = 'Pleno' | 'Cadete' | 'Honorario';
 export type MemberStatus = 'Activo' | 'Moroso' | 'Suspendido';
 
 export interface MemberDTO {
-  id: string; // UUID
-  dni: string;
-  name: string;
-  email: string;
-  birthdate: string; // ISO Date String (YYYY-MM-DD)
-  category: MemberCategory;
-  status: MemberStatus;
-  created_at: string; // ISO Date String
+    id: string; // UUID
+    dni: string;
+    name: string;
+    email: string;
+    birthdate: string; // ISO Date String (YYYY-MM-DD)
+    category: MemberCategory;
+    status: MemberStatus;
+    created_at: string; // ISO Date String
 }
 
 export interface CreateMemberRequest {
-  dni: string;
-  name: string;
-  email: string;
-  birthdate: string; // ISO Date String (YYYY-MM-DD)
-  category: MemberCategory;
+    dni: string;
+    name: string;
+    email: string;
+    birthdate: string; // ISO Date String (YYYY-MM-DD)
+    category: MemberCategory;
 }
 
 export interface UpdateMemberRequest {
-  dni?: string;
-  name?: string;
-  email?: string;
-  birthdate?: string; // ISO Date String (YYYY-MM-DD)
-  category?: MemberCategory;
-  status?: MemberStatus;
+    dni?: string;
+    name?: string;
+    email?: string;
+    birthdate?: string; // ISO Date String (YYYY-MM-DD)
+    category?: MemberCategory;
+    status?: MemberStatus;
 }
 
 // ==========================================
@@ -39,51 +39,57 @@ export interface UpdateMemberRequest {
 export type PaymentStatus = 'Pendiente' | 'Pagado' | 'Vencido' | 'Cancelado';
 
 export interface PaymentDTO {
-   id: string; //uuid
-   member_id: string; //FK uuid
-   amount: number;
-   month: number;
-   year: number;
-   due_date: string; // ISO Date String (YYYY-MM-DD)
-   payment_date: string | null; // ISO Date String (YYYY-MM-DD)
-   status: PaymentStatus;
-   member?: {
-    name: string;
-    dni: string;};
+    id: string; //uuid
+    member_id: string; //FK uuid
+    amount: number;
+    month: number;
+    year: number;
+    due_date: string; // ISO Date String (YYYY-MM-DD)
+    payment_date: string | null; // ISO Date String (YYYY-MM-DD)
+    status: PaymentStatus;
+    member?: {
+        name: string;
+        dni: string;
+    };
 }
 
 export interface CreatePaymentRequest {
-  member_id: string;
-  amount: number;
-  month: number;
-  year: number;
-  due_date: string; // ISO Date String (YYYY-MM-DD)
-  status?: PaymentStatus; // Opcional, por defecto 'Pendiente'
+    member_id: string;
+    amount: number;
+    month: number;
+    year: number;
+    due_date: string; // ISO Date String (YYYY-MM-DD)
+    status?: PaymentStatus; // Opcional, por defecto 'Pendiente'
+}
+
+export interface UpdatePaymentRequest {
+    status?: PaymentStatus;
+    payment_date?: string | null;
 }
 
 // ==========================================
 // Sport
 // ==========================================
 export interface SportDTO {
-  id: string;
-  name: string
-  description?: string;
-  max_capacity: number;
-  additional_price: number;
-  requires_medical_certificate: boolean;
+    id: string;
+    name: string;
+    description?: string;
+    max_capacity: number;
+    additional_price: number;
+    requires_medical_certificate: boolean;
 }
 
 export interface CreateSportRequest {
-  name: string;
-  description?: string;
-  max_capacity: number;
-  additional_price: number;
-  requires_medical_certificate: boolean;
+    name: string;
+    description?: string;
+    max_capacity: number;
+    additional_price: number;
+    requires_medical_certificate: boolean;
 }
 
 export interface UpdateSportRequest {
-  description?: string;
-  max_capacity?: number;
+    description?: string;
+    max_capacity?: number;
 }
 
 // ==========================================
@@ -91,17 +97,17 @@ export interface UpdateSportRequest {
 // ==========================================
 
 export interface CreateMedicalCertificateRequest {
-  member_id: string;
-  issue_date: Date | string;
-  expiry_date: Date | string;
-  doctor_license: string;
+    member_id: string;
+    issue_date: Date | string;
+    expiry_date: Date | string;
+    doctor_license: string;
 }
 
 export interface MedicalCertificateDTO {
-  id: string;
-  member_id: string;
-  issue_date: Date | string;
-  expiry_date: Date | string;
-  doctor_license: string;
-  is_validated: boolean;
+    id: string;
+    member_id: string;
+    issue_date: Date | string;
+    expiry_date: Date | string;
+    doctor_license: string;
+    is_validated: boolean;
 }
