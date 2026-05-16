fd0ec0c3be32ac4d1d83949e3a5daa1612016efc
Hajime Shiroma <hajishiro.hs@gmail.com>
2026-05-16 18:34:31 -0300

    feat(payment): agrega pantalla de creación de pago

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -1,26 +1,31 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from 'react-router';
 
-import { MembersView } from "./views/Members";
-import { DisciplinesView } from "./views/Disciplines";
-import { HomeView } from "./views/Home";
-import Layout from "./Layout";
+import { MembersView } from './views/Members';
+import { DisciplinesView } from './views/Disciplines';
+import { PaymentsView } from './views/Payments';
+import { HomeView } from './views/Home';
+import Layout from './Layout';
 
 export let router = createBrowserRouter([
-  {
-    Component: Layout,
-    children: [
-      {
-        path: "/",
-        Component: HomeView,
-      },
-      {
-        path: "/members",
-        Component: MembersView,
-      },
-      {
-        path: "/disciplines",
-        Component: DisciplinesView,
-      },
-    ],
-  },
+    {
+        Component: Layout,
+        children: [
+            {
+                path: '/',
+                Component: HomeView,
+            },
+            {
+                path: '/members',
+                Component: MembersView,
+            },
+            {
+                path: '/disciplines',
+                Component: DisciplinesView,
+            },
+            {
+                path: '/payments',
+                Component: PaymentsView,
+            },
+        ],
+    },
 ]);
