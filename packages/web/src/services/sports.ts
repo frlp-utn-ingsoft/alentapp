16740030ce863c628cca7f7bdf97c9ea8da650fe
yamartinez03 <yamartinez@frba.utn.edu.ar>
2026-05-17 11:26:43 -0300

    feat(Sport): agrega método delete en sportsService

--- packages/web/src/services/sports.ts
+++ packages/web/src/services/sports.ts
@@ -29,4 +29,14 @@ export const sportsService = {
     return result.data;
   },
 
+  async delete(id: string): Promise<void> {
+    const response = await fetch(`${API_URL}/sport/${id}`, {
+        method: 'DELETE',
+    });
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Error al eliminar el deporte');
+    }
+  },
+
 };
