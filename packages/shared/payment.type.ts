88afc760928dc17cb017cbe2e117fac6943c4b20
NahuelIroz <nahuel.t.p@hotmail.com>
2026-05-17 14:45:13 -0300

    feat(feature/payment-creae): Modificar pyment.type

--- packages/shared/payment.type.ts
+++ packages/shared/payment.type.ts
@@ -1,15 +1,6 @@
 export type PaymentStatus = 'Pending' | 'Paid' | 'Canceled' | 'Overdue';
 
-export interface Payment {
-  id: string;
-  amount: number;
-  month: number;
-  year: number;
-  status: PaymentStatus;
-  due_date: Date;
-  payment_date: Date | null;
-  member_id: string;
-}
+
 
 export interface CreatePaymentDTO {
   member_id: string;
