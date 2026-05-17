dcfc2027a34a61784c26a43c7c0168b9d54c3663
ThiagoDanielPerez <thiagodanielperez@alu.frlp.utn.edu.ar>
2026-05-17 14:12:33 -0300

    feat(backend): agrega update a interfaz DisciplineRepository

--- packages/api/src/domain/DisciplineRepository.ts
+++ packages/api/src/domain/DisciplineRepository.ts
@@ -1,7 +1,8 @@
-import { DisciplineDTO } from '@alentapp/shared';
+import { DisciplineDTO, UpdateDisciplineRequest } from '@alentapp/shared';
 
 export interface DisciplineRepository {
   create(data: Omit<DisciplineDTO, 'id'>): Promise<DisciplineDTO>;
   findById(id: string): Promise<DisciplineDTO | null>;
   findAll(): Promise<DisciplineDTO[]>;
+  update(id: string, data: UpdateDisciplineRequest): Promise<DisciplineDTO>;
 }
