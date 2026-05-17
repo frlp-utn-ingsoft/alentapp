24c2397cbf39b7cc24e8ab2d907909936449bffc
Melissa Braunstein <melissa.braunstein@gmail.com>
2026-05-16 21:49:25 -0300

    feat(payment): agregar payment a index.ts

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -31,3 +31,41 @@ export interface UpdateMemberRequest {
   category?: MemberCategory;
   status?: MemberStatus;
 }
+
+
+export type PaymentStatus = 'Pendiente' | 'Pagado' | 'Cancelado';
+
+export interface PaymentDTO {
+  id: string;
+  member_id: string;
+  amount: number;
+  month: number;
+  year: number;
+  status: PaymentStatus;
+  due_date: string;
+  payment_date: string | null;
+  created_at: string;
+  updated_at: string;
+  canceled_at: string | null;
+}
+
+export interface CreatePaymentRequest {
+  member_id: string;
+  amount: number;
+  month: number;
+  year: number;
+  due_date: string;
+}
+
+export interface UpdatePaymentRequest {
+  amount?: number;
+  due_date?: string;
+}
+
+export interface PaymentResponse {
+  data: PaymentDTO;
+}
+
+export interface PaymentsResponse {
+  data: PaymentDTO[];
+}
