3d1cd1fd4a6e946d281566a958fd1a5f630e3f07
Pilar <pilarwagner94@gmail.com>
2026-05-17 11:24:04 -0300

    feat(locker): agrega Use Case de consulta de lockers

---
+++ packages/api/src/application/GetLockersUseCase.ts
@@ -0,0 +1,10 @@
+import { LockerRepository } from '../domain/LockerRepository.js';
+import { LockerDTO } from '@alentapp/shared';
+
+export class GetLockersUseCase {
+    constructor(private readonly lockerRepository: LockerRepository) {}
+
+    async execute(): Promise<LockerDTO[]> {
+        return this.lockerRepository.findAll();
+    }
+}
