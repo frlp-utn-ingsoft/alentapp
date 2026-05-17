3de408aabcffe27980bcb74b7236691d36f90581
Pilar <pilarwagner94@gmail.com>
2026-05-17 11:25:54 -0300

    feat(locker): agrega ruta frontend de lockers

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 
 import { MembersView } from "./views/Members";
 import { HomeView } from "./views/Home";
+import { LockersView } from "./views/Lockers";
 import Layout from "./Layout";
 
 export let router = createBrowserRouter([
@@ -16,6 +17,10 @@ export let router = createBrowserRouter([
         path: "/members",
         Component: MembersView,
       },
+      {
+        path: "/lockers",
+        Component: LockersView,
+},
     ],
   },
 ]);
