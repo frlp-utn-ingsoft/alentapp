7dbe25d78a69c4b4e88b3458f1876c9d1497660a
Tomas Rosato <tomasrosato05@gmail.com>
2026-05-16 22:58:15 -0300

    feat(sport): agrega caso de uso para crear deporte

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -2,5 +2,6 @@ import { CreateSportRequest, SportDTO } from '@alentapp/shared';
 
 export interface SportRepository {
     create(sport: CreateSportRequest): Promise<SportDTO>;
+    findByName(name: string): Promise<SportDTO | null>;
     findById(id: string): Promise<SportDTO | null>;
 }
