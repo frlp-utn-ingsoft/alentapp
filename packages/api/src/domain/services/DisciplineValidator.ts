695f4120ff7538675a7516f06dc33322459c3c9b
Santiago Gonzalez D'Angelo <santiago.gonzalez@alu.frlp.utn.edu.ar>
2026-05-17 22:19:17 +0000

    feat(discipline): Agrega las validaciones para la eliminacion de disciplinas

--- packages/api/src/domain/services/DisciplineValidator.ts
+++ packages/api/src/domain/services/DisciplineValidator.ts
@@ -96,5 +96,13 @@ export class DisciplineValidator {
             this.validateDateFormat(date);
         }
     }
+    validateIdFormat(id: string): void {
+        const uuidRegex =
+            /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+        if (!uuidRegex.test(id)) {
+            throw new Error('Formato de ID inválido');
+        }
+    }
 }
 
