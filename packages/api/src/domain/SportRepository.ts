c5a92b3f89999be092bbae301220819df14768ac
Tomas Rosato <tomasrosato05@gmail.com>
2026-05-17 15:28:19 -0300

    feat(sport): amplia repositorio para consulta de deportes

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -1,6 +1,8 @@
-import { CreateSportRequest, SportDTO } from '@alentapp/shared';
+import { CreateSportRequest, SportDTO, GetSportsQuery } from '@alentapp/shared';
 
 export interface SportRepository {
     create(sport: CreateSportRequest): Promise<SportDTO>;
     findByName(name: string): Promise<SportDTO | null>;
+    findById(id: string): Promise<SportDTO | null>;
+    findAll(query?: GetSportsQuery): Promise<SportDTO[]>;
 }
