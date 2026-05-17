0ab6233f68dbac9d5455e469b8b7c7871d61bc8f
Ernesto Ardenghi <eardenghi@gmail.com>
2026-05-17 11:29:59 -0300

    refactor(payment): centralizacion de tipos/interfaces en index.ts
    Se mueve todo el contenido de packages/shared/payment.ts a index.ts para respetar la convención utilizada

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -60,10 +60,6 @@ export interface UpdateSportRequest {
   requires_medical_certificate?: boolean;
 }
 
-// ==========================
-// = Payments
-// ==========================
-export * from './payment.ts';
 
 // ==========================
 // = Medical Certificates
@@ -87,4 +83,27 @@ export interface MedicalCertificateDTO {
   deletedAt: string | null
 
   memberId: string
+}
+
+export type PaymentStatus = 'Pending' | 'Paid' | 'Canceled';
+
+export interface PaymentDTO {
+    id: string;
+    amount: number;
+    month: number;
+    year: number;
+    status: PaymentStatus;
+    due_date: string;
+    payment_date: string | null;
+    member_id: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface CreatePaymentRequest {
+    member_id: string;
+    amount: number;
+    month: number;
+    year: number;
+    due_date: string;
 }
